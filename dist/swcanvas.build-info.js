5587fa22b346332ed9861e12ba820dd5482f178a
Davide Della Casa <davidedc@gmail.com>
2026-01-29 12:23:11 +0000

    rebuild dist

--- dist/swcanvas.build-info.js
+++ dist/swcanvas.build-info.js
@@ -1,9 +1,9 @@
 // SWCanvas Build Information
-// Generated: 2026-01-28T19:29:21.020Z
+// Generated: 2026-01-29T12:14:40.980Z
 window.SWCANVAS_BUILD_INFO = {
-  "timestamp": "2026-01-28T19:29:21.020Z",
-  "commit": "54988e4",
-  "commitFull": "54988e421c5fa934b540166686a69692c09ada7b",
+  "timestamp": "2026-01-29T12:14:40.980Z",
+  "commit": "404a1ab",
+  "commitFull": "404a1ab70c784d2576eb8e92024cbaf08d26a59f",
   "branch": "main",
   "version": "1.0.0",
   "type": "dev"
